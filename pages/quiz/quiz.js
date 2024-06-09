b6e2e7d0bcdeb54c9eb6d91ee145df0f1fa6d53c
GustavoFerreiraturmac gustavo <gstvmorinico@gmail.com>
2024-06-09 18:58:14 +0000

    adicionadas as funçoes de buscar as perguntas e montar as perguntas no html

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -31,8 +31,8 @@ async function buscarPerguntas() {
     const urlDados = "../../data.json"
     
     await fetch(urlDados).then(resposta => resposta.json()).then(dados =>{
-        dados.quizzes.forEach(dado =>{
-             if  ( dado.title  ===  assunto )  {
+        dados.quizzes.forEach(dado => {
+             if (dado.title  ===  assunto) {
                 quiz  =  dado
              }
         })
@@ -45,59 +45,68 @@ function montarPergunta() {
     const main = document.querySelector("main")
 
     main.innerHTML = `
-    <section class="pergunta">
-    <div>
-        <p>Questão ${ pergunta } de 10</p>
-        <h2> ${ alterarSinais ( quiz . perguntas [ pergunta - 1 ] . pergunta ) } </h2>
-    </div>
-    <div class="barra_progresso">
-        <div style="width: ${ pergunta  *  10 } %"></div>
-    </div>
-</seção>
-<seção class="alternativas">
-    <form action="">
-        <label for="alternativa_a">
-            <input type="radio" id="alternativa_a" name="alternativa">
+            <section class="pergunta">
             <div>
-                <span>A</span>
-                ${ alterarSinais ( quiz . perguntas [ pergunta - 1 ] . opções [ 0 ] ) }
+                <p>Questão ${ pergunta } de 10</p>
+                <h2> ${ alterarSinais ( quiz . perguntas [ pergunta - 1 ] . pergunta ) } </h2>
             </div>
-        </label>
-        <label for="alternativa_b">
-            <input type="radio" id="alternativa_b" name="alternativa">
-            <div>
-                <span>B</span>
-                ${ alterarSinais ( quiz . perguntas [ pergunta - 1 ] . opções [ 1 ] ) }
+            <div class="barra_progresso">
+                <div style="width: ${ pergunta  *  10 } %"></div>
             </div>
-        </label>
-        <label for="alternativa_c">
-            <input type="radio" id="alternativa_c" name="alternativa">
-            <div>
-                <span>C</span>
-                ${ alterarSinais ( quiz . perguntas [ pergunta - 1 ] . opções [ 2 ] ) }
-            </div>
-        </label>
-        <label for="alternativa_d">
-            <input type="radio" id="alternativa_d" name="alternativa">
-            <div>
-                <span>D</span>
-                ${ alterarSinais ( quiz . perguntas [ pergunta - 1 ] . opções [ 3 ] ) }
-            </div>
-        </label>
-    </form>
-    <button>Enviar</button>
-</seção>
-`
+        </seção>
+        <seção class="alternativas">
+            <form action="">
+                <label for="alternativa_a">
+                    <input type="radio" id="alternativa_a" name="alternativa" value="${ alterarSinais ( quiz . perguntas [ pergunta - 1 ] . opções [ 0 ] ) }">
+                    <div>
+                        <span>A</span>
+                        ${ alterarSinais ( quiz . perguntas [ pergunta - 1 ] . opções [ 0 ] ) }
+                    </div>
+                </label>
+                <label for="alternativa_b">
+                    <input type="radio" id="alternativa_b" name="alternativa" value="${ alterarSinais ( quiz . perguntas [ pergunta - 1 ] . opções [ 1 ] ) }">
+                    <div>
+                        <span>B</span>
+                        ${ alterarSinais ( quiz . perguntas [ pergunta - 1 ] . opções [ 1 ] ) }
+                    </div>
+                </label>
+                <label for="alternativa_c">
+                    <input type="radio" id="alternativa_c" name="alternativa" value="${ alterarSinais ( quiz . perguntas [ pergunta - 1 ] . opções [ 2 ] ) }">
+                    <div>
+                        <span>C</span>
+                        ${ alterarSinais ( quiz . perguntas [ pergunta - 1 ] . opções [ 2 ] ) }
+                    </div>
+                </label>
+                <label for="alternativa_d">
+                    <input type="radio" id="alternativa_d" name="alternativa" value="${ alterarSinais ( quiz . perguntas [ pergunta - 1 ] . opções [ 3 ] ) }">
+                    <div>
+                        <span>D</span>
+                        ${ alterarSinais ( quiz . perguntas [ pergunta - 1 ] . opções [ 3 ] ) }
+                    </div>
+                </label>
+            </form>
+            <button>Enviar</button>
+        </seção>
+    `
 }
 
 function alterarSinais(texto) {
     return texto.replace(/</g, "&lt;").replace(/>/g, "&gt;")
 }
 
-função  assíncrona iniciar ( )  {
-alterarAssunto ( )
-aguarde  buscarPerguntas ( )
-montarPergunta ( )
+function guardarResposta() {
+    console.log("guardar resposta")
 }
 
-iniciar ( )
+async function iniciar()  {
+    alterarAssunto()
+    await buscarPerguntas()
+    montarPergunta()
+
+    const inputsResposta = document.querySelectorAll(".alternativas input")
+    inputsResposta.forEach(input => {
+        input.addEventListener("click", guardarResposta)
+    })
+}
+
+iniciar()
